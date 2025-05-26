eca174d4faa231faf8da3a02e93f1a346ee0020f
oskarhermanowski <oskar.hermanowski@gmail.com>
2025-05-26 23:24:21 +0200

    test11

--- src/axios.jsx
+++ src/axios.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: 'https://node-db-main.onrender.com'
+    baseURL: 'https://node-db-main.onrender.com/api'
 });
 
 export default instance;
