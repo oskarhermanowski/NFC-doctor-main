646d2ca2d5fcf73f1f0857d05327d2337cc43b90
oskarhermanowski <oskar.hermanowski@gmail.com>
2025-05-27 18:23:03 +0200

    gettaginfo

--- src/components/Nfcs/ReadTag/GetTagInfo.jsx
+++ src/components/Nfcs/ReadTag/GetTagInfo.jsx
@@ -1,46 +1,38 @@
 import logReadTag from "./LogReadTag";
 import axios from "../../../axios";
-import logReadTagInfo from "./LogReadTagInfo"
+import logReadTagInfo from "./LogReadTagInfo";
 import { getDateAndTime } from "../../Date/GetDateAndTime";
 
 export default async function getTagInfo(event) {
   const decoder = new TextDecoder();
+  const record = event.message.records[0];
+  const id = decoder.decode(record.data);
+  const readTime = getDateAndTime();
+
   logReadTagInfo("Reading tag... Step[2/3]");
-  for (const record of event.message.records) {
-    var dateTime = getDateAndTime();
-    axios.get('/nfcs/' + decoder.decode(record.data)).then(function(result){
-      if(result.data.batchNumber != undefined){
-        logReadTagInfo("Success!");
-        if(result.data.index != undefined) {
-          logReadTag("Product information:\n" + result.data.info.toString() + "\nIndex: " + result.data.index.toString() +
-          "\nBatch number: " + result.data.batchNumber.toString() + "\nTagged at: " + result.data.timeStamp.toString() +
-          "\nRead at: " + dateTime);
-        }
-        else {
-          logReadTag("Product information:\n" + result.data.info.toString() + "\nBatch number: " +
-          result.data.batchNumber.toString() + "\nTagged at: " + result.data.timeStamp.toString() + "\nRead at: " + dateTime);
-        }
-      }
-      else{
-        logReadTagInfo("Success!");
-        if(result.data.index != undefined) {
-          logReadTag("Product information:\n" + result.data.info.toString() + "\nIndex: " + result.data.index.toString() +
-          "\nTagged at: " + result.data.timeStamp.toString() + "\nRead at: " + dateTime);
-        }
-        else {
-          logReadTag("Product information:\n" + result.data.info.toString() + "\nTagged at: " +
-          result.data.timeStamp.toString() + "\nRead at: " + dateTime);
-        }
-      }
-    }).catch(err => {
-        if (err.name == 'TypeError') {
-          logReadTagInfo("Oops!");
-          logReadTag("This tag is no longer in our database.");
-        }
-        else {
-          logReadTagInfo("This tag is not defined in our database.");
-          logReadTag('Message saved on this tag:\n' + decoder.decode(record.data));
-        }
-      });
-  }    
+
+  try {
+    const res = await axios.get('/nfcs/' + id);
+    const data = res.data;
+
+    logReadTagInfo("Success!");
+    logReadTag(
+      `Pacjent:\n` +
+      `Imię: ${data.firstName}\n` +
+      `Nazwisko: ${data.lastName}\n` +
+      `PESEL: ${data.pesel}\n` +
+      `Data urodzenia: ${data.birthDate}\n` +
+      `Grupa krwi: ${data.bloodType}\n` +
+      `Alergie: ${data.allergies}\n` +
+      `Leki: ${data.medications}\n` +
+      `Choroby przewlekłe: ${data.chronicDiseases}\n` +
+      `Lekarz: ${data.assignedDoctorId}\n` +
+      `Zapisano: ${data.timeStamp}\n` +
+      `Odczytano: ${readTime}`
+    );
+
+  } catch (err) {
+    logReadTagInfo("❌ Nie znaleziono rekordu w bazie danych");
+    logReadTag(`Wartość z tagu:\n${id}`);
+  }
 }
