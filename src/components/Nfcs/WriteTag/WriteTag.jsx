d9ffb726e1d307874a4d401e92cfeaea46a6acbb
oskarhermanowski <oskar.hermanowski@gmail.com>
2025-05-27 16:50:21 +0200

    test19

--- src/components/Nfcs/WriteTag/WriteTag.jsx
+++ src/components/Nfcs/WriteTag/WriteTag.jsx
@@ -6,11 +6,8 @@ import { enableButtons } from "../../ButtonActions/EnableButtons";
 import { getDateAndTime } from "../../Date/GetDateAndTime";
 
 export default async function writeTag(formData, retries = 2) {
-    const ndef = new NDEFReader();
     
     const response = await axios.post("/nfcs", {
       ...formData,
 });
-var id = res.data._id.toString();
- await ndef.write(id);
 }
